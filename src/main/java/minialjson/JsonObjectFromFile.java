78d2c37160c3f2d46f02b9d18025f82bbf3b0045
toandv <dovantoanhp@gmail.com>
2017-04-27 22:36:32 +0700

    init

---
+++ src/main/java/minialjson/JsonObjectFromFile.java
@@ -0,0 +1,57 @@
+package minialjson;
+
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by dovan on 4/27/2017.
+ */
+public class JsonObjectFromFile {
+    static String json = "{\n" +
+            "  \"array\": [\n" +
+            "    1,\n" +
+            "    2,\n" +
+            "    3\n" +
+            "  ],\n" +
+            "  \"boolean\": true,\n" +
+            "  \"null\": null,\n" +
+            "  \"number\": 123,\n" +
+            "  \"object\": {\n" +
+            "    \"a\": \"b\",\n" +
+            "    \"c\": \"d\",\n" +
+            "    \"e\": \"f\"\n" +
+            "  },\n" +
+            "  \"string\": \"Hello World\"\n" +
+            "}";
+
+    static File createFile(String fileName, String content) throws IOException {
+        File file = new File(fileName);
+        file.createNewFile();
+        try (FileOutputStream out = new FileOutputStream(file)) {
+            out.write(content.getBytes());
+        }
+        return file;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        File jsonFile = createFile("test", json);
+        try (FileReader reader = new FileReader(jsonFile)) {
+            JsonObject jsonObject = Json.parse(reader).asObject();
+            System.out.println("String: " + jsonObject.get("string").asString());
+            System.out.println("Number: " + jsonObject.get("number").asInt());
+            JsonArray array = jsonObject.get("array").asArray();
+            System.out.println("Array: ");
+            array.forEach(each -> System.out.print(each.asInt() + " "));
+            JsonObject object = jsonObject.get("object").asObject();
+            System.out.println("\nObject: " + object);
+
+        }
+    }
+}
